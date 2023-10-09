c32b93de6fa5065fc4caae28d459a97ce913fc88
Ubuntu <ubuntu@ip-172-31-14-234.ap-south-1.compute.internal>
2023-10-09 17:34:08 +0000

    shell script

---
+++ trial.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+echo Hello World...
+institute="IET"
+echo CDAC authorized  $name center
+
+echo "Enter name:"
+read uname
+echo Hye $uname,how\'s the day?
+
+
+#echo "Enter file name to create"
+#read file_nm
+#touch "${file_nm}"
+
+#for i in hello 1 * 2 goodbye
+#for i in 1 2 3 4 5
+#do
+#  echo "Looping ... i is set to $i"
+#done
+
+#choice=yes
+#while [ "$choice" != "no" ]
+#do
+#  echo "Enter yes to continue else no"
+#  read choice
+#  echo "You typed: $choice"
+#done
+
+#echo "Enter:"
+#while read input_text
+#do
+#  case $input_text in
+#        hello)          echo English    ;;
+#        howdy)          echo American   ;;
+#        gday)           echo Australian ;;
+#        bonjour)        echo French     ;;
+#        "guten tag")    echo German     ;;
+#        *)              echo Unknown Language: $input_text
+#                ;;
+#   esac
+#done  #ctrl+c
+
+
+
